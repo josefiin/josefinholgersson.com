1b63663477690e571425fdc482f57a47eafc3674
josefiin <146443336+josefiin@users.noreply.github.com>
2026-01-11 16:36:55 +0100

    update

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,11 +3,13 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'no50snex',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
-  autoUpdates: true,
+  deployment: {
+    autoUpdates: true,
+  },
 })
